be3860ec1f03990b3170c8441db22a86e102f210
P4warit <142296227+P4warit06@users.noreply.github.com>
2025-02-10 14:36:18 +0700

    Commit song
    Commit

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,149 +1,60 @@
 <script setup>
-import { ref, computed, watch } from 'vue'
-const agreement = ref(false) //single checkbox (true/false)
-const favorConverters = ref([]) //multiple checkbox (multitple values)
-const unitType = ref('length') //single value of radio button
-const feedback = ref('')
-// const selectOptions = computed(() => {
-//   if (unitType.value === 'length')
-//     return `<option value="centimetre">Centimetre</option>
-//             <option value="inch">Inch</option>`
-//   else if (unitType.value === 'temperature')
-//     return `<option value="celsius">Celsius</option>
-//             <option value="fahrenheit">Fahrenheit</option>`
-// })
-const selectOptions = ref('')
-const sourceUnit = ref('')
-const targetUnit = ref('')
-const sourceValue = ref(1)
-// const targetValue = ref(1)
-
-const targetValue = computed(() => {
-  if (
-    unitType.value === 'length' &&
-    sourceUnit.value === 'centimetre' &&
-    targetUnit.value === 'inch'
-  )
-    return (sourceValue.value / 2.54).toFixed(2)
-  else if (
-    unitType.value === 'length' &&
-    sourceUnit.value === 'inch' &&
-    targetUnit.value === 'centimetre'
-  )
-    return (sourceValue.value * 2.54).toFixed(2)
-  else if (
-    unitType.value === 'temperature' &&
-    sourceUnit.value === 'celsius' &&
-    targetUnit.value === 'fahrenheit'
-  )
-    return (sourceValue.value * (9 / 5) + 32).toFixed(2)
-  else if (
-    unitType.value === 'temperature' &&
-    sourceUnit.value === 'fahrenheit' &&
-    targetUnit.value === 'celsius'
-  )
-    return ((sourceValue.value - 32) * (5 / 9)).toFixed(2)
-  else return sourceValue.value
-})
-watch(
-  unitType,
-  () => {
-    if (unitType.value === 'length') {
-      selectOptions.value = `<option value="centimetre">Centimetre</option>
-            <option value="inch">Inch</option>`
-      sourceUnit.value = 'centimetre'
-      targetUnit.value = 'inch'
-    } else if (unitType.value === 'temperature') {
-      selectOptions.value = `<option value="celsius">Celsius</option>
-            <option value="fahrenheit">Fahrenheit</option>`
-      sourceUnit.value = 'celsius'
-      targetUnit.value = 'fahrenheit'
-    }
-  },
-  { immediate: true }
-)
+import { ref } from 'vue'
+const musicPlayer = ref('starting')
+console.log(musicPlayer.value)
+const onMusic = ref(false)
+const playMusic = () => {
+  onMusic.value = !onMusic.value
+  console.log(musicPlayer.value)
+  if (onMusic.value) musicPlayer.value.play()
+  else musicPlayer.value.pause()
+}
 </script>
 
 <template>
-  <div class="w-full max-w-3xl p-10 shadow-2xl space-y-2">
-    <!-- converter form -->
-    <div>
-      <h1 class="text-green-700 text-3xl">Converter</h1>
-    </div>
 
-    <form action="" class="space-y-2">
-      <div class="flex space-x-3">
-        <input type="checkbox" v-model="agreement" />
-        <p>I read and accept converter application policy</p>
-      </div>
+  
+  <div>
+    <div class="flex space-x-1 items-center">
+      <p>Music:</p>
+      <label class="swap">
+        <!-- this hidden checkbox controls the state -->
+        <input type="checkbox" @click="playMusic" v-model="onMusic" />
 
-      <div class="flex space-x-3">
-        <p>Favorite Converters:</p>
-        <input
-          type="checkbox"
-          value="unit converter"
-          v-model="favorConverters"
-        />
-        <label>Unit Converter</label>
-        <input
-          type="checkbox"
-          value="conversion calculator"
-          v-model="favorConverters"
-        />
-        <label>Conversion Calculator</label>
-      </div>
-
-      <div class="flex space-x-3">
-        <input type="radio" value="length" v-model="unitType" />
-        <label>Length</label>
-        <input type="radio" value="temperature" v-model="unitType" />
-        <label>Temperature</label>
-      </div>
-
-      <div class="flex space-x-3 items-center">
-        <div class="w-1/2 flex flex-col">
-          <input
-            v-model.number="sourceValue"
-            type="text"
-            value="1"
-            class="text-center h-20 text-2xl border border-gray-200 outline-none"
+        <!-- volume on icon -->
+        <svg
+          class="swap-on fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"
           />
-          <!-- dynamic options -->
-          <select
-            v-model="sourceUnit"
-            v-html="selectOptions"
-            class="bg-gray-50 border border-slate-300 outline-none"
-          ></select>
-        </div>
-        <p class="text-3xl">=</p>
-        <div class="w-1/2 flex flex-col">
-          <div
-            class="flex justify-center items-center h-20 text-2xl border border-gray-200"
-          >
-            <label>{{ targetValue }}</label>
-          </div>
+        </svg>
 
-          <!-- dynamic options -->
-          <select
-            v-model="targetUnit"
-            v-html="selectOptions"
-            class="bg-gray-50 border border-slate-300 outline-none"
-          ></select>
-        </div>
-      </div>
-      <div class="flex flex-col">
-        <label>Give us your feedback:</label>
-        <textarea
-          v-model="feedback"
-          class="border border-slate-300 outlne-none max-h-10"
-        ></textarea>
-      </div>
-      <button
-        class="bg-green-700 text-white hover:bg-green-600 rounded-lg px-5 py-1"
-      >
-        Submit
-      </button>
-    </form>
+        <!-- volume off icon -->
+        <svg
+          class="swap-off fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z"
+          />
+        </svg>
+      </label>
+      <audio controls class="hidden" ref="musicPlayer">
+        <source src="./assets/sample.mp3" type="audio/mp3" />
+        <p>
+          Your browser doesn't support this audio file. Here is a
+          <a href="sample.mp3">link to the audio</a> instead.
+        </p>
+      </audio>
+    </div>
   </div>
 </template>
 
