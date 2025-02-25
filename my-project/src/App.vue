2ac853dbb239a3b0e5a679cfd59ce0f4e82f5566
P4warit <142296227+P4warit06@users.noreply.github.com>
2025-02-25 15:41:32 +0700

    Test Demo song

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,151 +1,157 @@
 <script setup>
-import { ref } from 'vue'
-import { previewBinaryFile } from './assets/utilities' 
-const musicPlayer = ref('starting')
-console.log(musicPlayer.value)
+import { onMounted, ref } from 'vue'
+import { previewBinaryFile } from './assets/utilities'
+
+const musicPlayer = ref(null) // Initialize to null
 const onMusic = ref(false)
+const volume = ref(0.5)
+
 const playMusic = () => {
   onMusic.value = !onMusic.value
-  console.log(musicPlayer.value)
-  if (onMusic.value) musicPlayer.value.play()
-  else musicPlayer.value.pause()
+  if (onMusic.value) {
+    musicPlayer.value.play().catch(error => {
+      console.error('Error playing music:', error)
+    })
+  } else {
+    musicPlayer.value.pause()
+  }
 }
-const urlDocFile =ref('') 
-const showPicture = ref('') 
+const loopMusic = () => {
+  if (musicPlayer.value) {
+    musicPlayer.value.loop = true;
+  }
+};
+
+onMounted(() => {
+  if (musicPlayer.value) {
+    musicPlayer.value.load();
+    musicPlayer.value.play().catch(error => {
+      console.error("Error playing music:", error);
+    });
+
+    loopMusic();
+  } else {
+    console.error("musicPlayer");
+  }
+});
+const changeVolume = () => {
+  musicPlayer.value.volume = volume.value
+}
+
+onMounted(() => {
+  // Check if musicPlayer.value is defined before accessing it
+  if (musicPlayer.value) {
+    musicPlayer.value.load();
+    // Autoplay, handle potential errors
+    musicPlayer.value.play().catch(error => {
+      console.error("Error playing music:", error);
+      // You might want to handle this error gracefully, e.g., display a message to the user
+    });
+  } else {
+    console.error("musicPlayer ref is not yet available in onMounted");
+  }
+});
+
+
+const urlDocFile = ref('')
+const showPicture = ref('')
 const docName = ref('')
-const chooseBinaryFile = (event) => { 
-// console.log(event.target.value);
-// console.log(event.target);
-// console.log(event.target.files);
-// console.log(event.target.files[0]);
-// console.log(event.target.files[0].name);
-docName.value = event.target.files[0].name
-if(docName.value.endsWith('.pdf') || docName.value.endsWith('.doc')){ 
-  urlDocFile.value = previewBinaryFile(event.target.files[0])
-}
-  else if(docName.value.endsWith('.jpg') || docName.value.endsWith('.png') || docName.value.endsWith('.jpeg')) { 
-        showPicture.value = URL.createObjectURL(file);
-    }
-    return {showPicture , chooseBinaryFile} 
+
+const chooseBinaryFile = (event) => {
+  docName.value = event.target.files[0].name
+  if (docName.value.endsWith('.pdf') || docName.value.endsWith('.doc')) {
+    urlDocFile.value = previewBinaryFile(event.target.files[0])
+  } else if (docName.value.endsWith('.jpg') || docName.value.endsWith('.png') || docName.value.endsWith('.jpeg')) {
+    showPicture.value = URL.createObjectURL(event.target.files[0]);
+  }
 }
 </script>
 
 <template>
   <section class="sm:mt-6 lg:mt-8 mt-12 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-<div
-    class="my-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 flex gap-3 lg:flex-justify lg:flex flex-col lg:flex-row">
-    <div class="sm:text-center lg:text-left">
+    <div
+      class="my-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 flex gap-3 lg:flex-justify lg:flex flex-col lg:flex-row">
+      <div class="sm:text-center lg:text-left">
         <h1 class="text-4xl tracking-tight font-extrabold text-gray-800 sm:text-5xl md:text-6xl">
-            <span class="block xl:inline">Data to enrich your</span>
-            <span class="block text-indigo-600 xl:inline">online business</span>
+          <span class="block xl:inline">Data to enrich your</span>
+          <span class="block text-indigo-600 xl:inline">online business</span>
         </h1>
-        <p
-            class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-            fugiat veniam occaecat fugiat aliqua.
+        <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+          fugiat veniam occaecat fugiat aliqua.
         </p>
-        <!-- Button Section -->
-        <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-            <div class="rounded-md shadow">
-                <a href="#"
-                    class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-600 md:py-4 md:text-lg md:px-10">
-                    Get started
-                </a>
-            </div>
-            <div class="mt-3 sm:mt-0 sm:ml-3">
-                <a href="#"
-                    class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-gray-800 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
-                    Live demo
-                </a>
-            </div>
-        </div>
-        <!-- End of Button Section -->
+      </div>
     </div>
+  </section>
 
-    <!--   Image Section     -->
-    <div class="lg:inset-y-0 lg:right-0 lg:w-1/2 my-4">
-        <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="">
-    </div>
-    <!--   End of Image Section     -->
+  <div class="flex">
+  <div class="flex rounded-t-lg  rounded-b-lg flex-col grid-col-3  items-center bg-red-100 p-1.5 ml-330">
+    <p>Music:</p>
+    <input type="range" min="0" max="1" step="0.01" v-model="volume" @input="changeVolume"
+      class="w-28 h-28 rotate-270" />
 
-
-    
-</div>
-
-</section>
-<div class="bg-blue-400 border-2 border-white shadow-lg rounded-xl p-6 text-center text-white max-w-md mx-auto">
-    <div class="text-2xl font-bold text-black"> h2 Template </div>
-</div>
-
-<!-- music--> 
-    <div>
-    <div class="flex space-x-1 items-center">
-      <p>Music:</p>
+    <div class="flex bg-blue-200 p-3">
       <label class="swap">
-        <!-- this hidden checkbox controls the state -->
         <input type="checkbox" @click="playMusic" v-model="onMusic" />
-
-        <!-- volume on icon -->
-        <svg
-          class="swap-on fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
+        <svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+          viewBox="0 0 24 24">
           <path
-            d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"
-          />
+            d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z" />
         </svg>
-
-        <!-- volume off icon -->
-        <svg
-          class="swap-off fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
+        <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+          viewBox="0 0 24 24">
           <path
-            d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z"
-          />
+            d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z" />
         </svg>
       </label>
+
       <audio controls class="hidden" ref="musicPlayer">
         <source src="./assets/sample.mp3" type="audio/mp3" />
-        <p>
-          Your browser doesn't support this audio file. Here is a
-          <a href="sample.mp3">link to the audio</a> instead.
+        <p>Your browser doesn't support this audio file. Here is a <a href="sample.mp3">link to the audio</a> instead.
         </p>
       </audio>
     </div>
   </div>
+</div>
 
 
-       
-<div>
-  <!-- <div class="">
-    Attach File : <input type="file" accept=".pdf , .doc"@change="chooseBinaryFile"> 
-    <a href="urlDocFile" target="_blank" class="bg-red-400" >{{docName}} </a>
-  </div> -->
+
+
+
+  <!-- Ui 
+  <div class="flex space-x-2 items-center">
+  <p>Music:</p>
+  <label class="swap">
+    <input type="checkbox" @click="playMusic" v-model="onMusic" />
+    <svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z"/>
+    </svg>
+    <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"/>
+    </svg>
+  </label>
+
+  Vertical Slider -->
+  <!-- <input type="range" min="0" max="1" step="0.01" v-model="volume" @input="changeVolume" class="w-24 h-48 rotate-270" />
+
+  <audio controls class="hidden" ref="musicPlayer">
+    <source src="./assets/sample.mp3" type="audio/mp3" />
+    <p>Your browser doesn't support this audio file. Here is a <a href="sample.mp3">link to the audio</a> instead.</p>
+  </audio> -->
+  <!-- </div> -->
+
+
+
+
   <div class="p-4">
     <div>
-      <h2 class="red"> Picture : <input type="file" @change="chooseBinaryFile" accept="image/*" class="mb-4 " /> </h2>
+      <h2 class="red">Picture : <input type="file" @change="chooseBinaryFile" accept="image/*" class="mb-4 " /></h2>
     </div>
-   
     <div v-if="showPicture" class="thumbnail-container">
       <p>🖼 รูปภาพที่เลือก:</p>
       <img :src="showPicture" target="_blank" class="bg-red-500" />
     </div>
   </div>
-
-
-</div>
-
-
-
-
-
 </template>
 
 <style scoped></style>
